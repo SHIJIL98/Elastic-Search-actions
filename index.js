2b8be41f785a7a125fded6d01994a76b8d80be3f
SHIJIL K <shijilkailas@gmail.com>
2019-02-21 02:03:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
-dropArticle();
-
-function dropArticle(){
-    var AWSES = require("aws-es");
-    var esconfig = require('./prod-config');
-    var es = new AWSES(esconfig);
-    
-    var table = 'task';
-
-    es.delete({
-        index: table.toLocaleLowerCase()
-    }, function (err, data) {
-        console.log("out: %j",[err, data]);
- });
-}
-function createIndex(){
-    var AWSES = require("aws-es");
-    var esconfig = require('./prod-config');
-    var es = new AWSES(esconfig);
-    
-    var table = 'dev_activityreport';
-
-    es.createIndex({
-        index: table.toLocaleLowerCase()
-    }, function (err, data) {
-        console.log("out: %j",[err, data]);
- });
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client(require('./config'));  
+function checkHealth() {
+    client.indices.exists({ index: 'gov' }, function (err, resp, status) {
+        if (err) {
+            console.log("-- Client Health error --", err);
+        }
+        console.log("-- Client Health --", resp);
+    });
 }
 
+function createIndex() {
+    client.indices.create({
+        index: 'gov'
+    }, function (err, resp, status) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("create", resp);
+        }
+    });
+  
+}
+function deleteIndex() {
+    client.indices.delete({ index: 'vendorreport' }, function (err, resp, status) {
+        console.log("delete", resp);
+    });
+}
+function deletedata() {
+    client.delete({  
+        index: 'taskreport',
+        id: '2018-02053',
+        type: 'taskreport'
+      }, function (err, resp, status) {
+        console.log("delete", resp);
+    });
+}
+
+function addDataToIndex(){
+    client.index({  
+        index: 'gov',
+        id: '1',
+        type: 'constituencies',
+        body: {
+          "ConstituencyName": "Ipswich",
+          "ConstituencyID": "E14000761",
+          "ConstituencyType": "Borough",
+          "Electorate": 74499,
+          "ValidVotes": 48694,
+        }
+      },function(err,resp,status) {
+          console.log(resp);
+      });
+      client.count({index: 'gov',type: 'constituencies'},function(err,resp,status) {  
+        console.log("constituencies",resp);
+      });    
+}
